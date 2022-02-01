9a3088ae9465f9889e7ca1728c5654e1a659dcdb
Calvin Simpfendorfer <calvin@nomoss.co>
2022-02-01 11:48:05 +1100

    Bookmarklet version

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-function getCSV() {
-  const input = document.getElementById("jsonData").value;
+//const input = document.getElementById("jsonData").value;
+const input = document.getElementsByTagName("pre")[0].textContent;
 
-  const jsonData = JSON.parse(input.substr(input.indexOf("{")));
+const jsonData = JSON.parse(input.substr(input.indexOf("{")));
 
-  let csvContent = "data:text/csv;charset=utf-8,";
+let csvContent = "data:text/csv;charset=utf-8,";
 
-  jsonData.list.forEach((el) => {
-    csvContent = csvContent + el.name + ", " + el.id + "\n";
-  });
+jsonData.list.forEach((el) => {
+  csvContent = csvContent + el.name + ", " + el.id + "\n";
+});
 
-  var encodedUri = encodeURI(csvContent);
-  var link = document.createElement("a");
-  link.setAttribute("href", encodedUri);
-  link.setAttribute("download", "data.csv");
-  document.body.appendChild(link); // Required for FF
+var encodedUri = encodeURI(csvContent);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", "data.csv");
+document.body.appendChild(link); // Required for FF
 
-  link.click(); // This will download the data file named "my_data.csv".
-}
+link.click();
